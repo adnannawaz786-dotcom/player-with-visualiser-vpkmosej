c8d8f3d80325462acb8c24a989a98dd5cbdb5b61
adnannawaz786-dotcom <adnan.nawaz786@ymail.com>
2025-08-12 12:36:28 +0100

    Add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+```jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './styles/global.css'
+
+// Initialize audio context on user interaction to comply with browser policies
+let audioContextInitialized = false;
+
+const initializeAudioContext = () => {
+  if (!audioContextInitialized) {
+    // This will be handled by the audioContext utility
+    audioContextInitialized = true;
+    document.removeEventListener('click', initializeAudioContext);
+    document.removeEventListener('touchstart', initializeAudioContext);
+  }
+};
+
+// Add event listeners for user interaction
+document.addEventListener('click', initializeAudioContext);
+document.addEventListener('touchstart', initializeAudioContext);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+```
